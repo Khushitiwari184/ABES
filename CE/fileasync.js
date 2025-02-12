99b1363dc39eb0e468339bed79a36e4e1d33132b
KT <khushimona@184gmail.com>
2025-02-12 09:28:44 +0530

    my work

---
+++ CE/fileasync.js
@@ -0,0 +1,15 @@
+const fs=require("fs/promises");
+const read = async () => {
+    const data = await fs.readFile("./data.text","utf-8");
+    console.log(data);
+}
+const write = async () => {
+    const newdata ="Hello World";
+    await fs.writeFile("./data1.txt",newdata ,"utf-8");
+    
+}
+write();
+// read();
+// console.log("first");
+// console.log("second");
+// console.log("third");   
